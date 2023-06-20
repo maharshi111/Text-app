ea87b2ee03f8b6168a0f9ccdbc50c162a912848c
maharshi111 <97938886+maharshi111@users.noreply.github.com>
2023-06-21 01:25:25 +0530

    text app 12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+import Navbar from  './Components/Navbar'// src\Components\Navbar.js
+import TextForm from './Components/TextForm';
+//import About from  './Components/About'
 
 function App() {
+  const[mode,setMode] = useState('light');//whether dark mode is enabeled or not 
+  const toggle=()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor= "#383842";
+    
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor= "white";
+      
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title = 'TextUtils' mode={mode} toggleMode = {toggle}/>
+    <div className="container my-3">
+    {/* container is class of bootstrap that places the text box in the center */}
+     <TextForm heading = "Enter the text to analyze below"  mode={mode}  />
+    {/* <About /> */}
+      
     </div>
+    
+    </>
+  
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,66 @@
+
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+  const[text,setText] = useState("");
+  function handleUPClick( ){
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  function handleOnChange(event){
+    setText(event.target.value);
+  }
+  function handleDownClick( ){
+     setText(text.toLowerCase());
+  }
+
+  function special( ){
+    const countSpecial = text => {
+      const punct = "!,.-?";
+      let count = 0;
+      for(let i = 0; i < text.length; i++){
+         if(!punct.includes(text[i])){
+            continue;
+         };
+         count++;
+      };
+      return count;
+   };
+   alert( "The number of special characters are "+countSpecial(text));
+ }
+
+ function clearClick( ){
+  setText("");
+}
+ 
+  
+  // function handleDownClick(event){
+  //   event.preventDefault();
+  //   setText(event.target.value.toLowerCase())
+  // }
+  return (
+    <>
+      
+      <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+          <h1>{props.heading} </h1>
+          <div className="mb-3">
+          <textarea className="form-control" id="myBox" value= {text} style={{backgroundColor: props.mode==='light'?'white':'grey',color:props.mode==='light'?'black':'white'}} onChange={handleOnChange} rows="8"></textarea>
+          </div>
+          <button className="btn btn-primary mx-1" onClick={handleUPClick}>convert to uppercase</button>
+          <button className="btn btn-primary mx-1" onClick={handleDownClick} >convert to lowercase</button>
+          <button className="btn btn-primary mx-1" onClick={special} >count special charcters</button>
+          <button className="btn btn-primary mx-1" onClick={clearClick} >clear</button>
+          
+      </div>
+      <div className={`container my-3  text-${props.mode==='light'?'dark':'light'}`} >{/* my-3 is also a class of bootstrap that means margin in y of 3  and text is a bootstrap property in case if you want to change the font color to white you can also use the method of the above div*/}
+         <h2>Your text summary </h2>
+         <p>{text.split(" ").length} words and {text.length} characters</p>
+         <p>{0.008*text.split(" ").length} minutes of reading</p>
+         <h2>preivew</h2>
+         <p>{text.length>0?text:"please enter something to preview"}</p>
+      </div>
+       
+      
+    </>
+  )
+}
